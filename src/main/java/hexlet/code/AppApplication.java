12e54d360789b10e5b1dd40656b557288cdcfca5
Igor Pavlov <bujhm9987@gmail.com>
2023-11-21 22:43:07 +0300

    add Sentry

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task Manager",
+                version = "1.0",
+                description = "Task management system"
+        )
+)
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
