b4a5afbdbbe8484abbe3e2adac01930a0a482226
Igor Pavlov <bujhm9987@gmail.com>
2023-11-23 23:14:25 +0300

    refactoring test

--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -10,8 +10,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
